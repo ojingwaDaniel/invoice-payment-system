5a2a96479092ee5153d0444f7c35be3b9b28accb
Daniel Ojingwa <ojingwadanny@gmail.com>
2025-11-21 16:46:10 +0100

    added changes to the dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -203,19 +203,7 @@
                             </div>
                         </div>
 
-                        <div
-                            class="flex items-center justify-between rounded-xl bg-gray-50 p-4 transition-colors hover:bg-purple-50">
-                            <div class="flex items-center">
-                                <div class="mr-4 flex h-10 w-10 items-center justify-center rounded-xl bg-purple-100">
-                                    <svg class="h-5 w-5 text-purple-600" fill="none" stroke="currentColor"
-                                        viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
-                                        </path>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
 
@@ -469,7 +457,7 @@
                     </div>
                 </div>
 
-        
+
             </div>
 
             <!-- Top Customers -->
